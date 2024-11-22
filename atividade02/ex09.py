9a334360db9d3c14de21673960a040f5dcbb392c
ricardo8santana <luisriccardo@proton.me>
2024-11-22 11:53:34 -0300

    python

---
+++ atividade02/ex09.py
@@ -0,0 +1,10 @@
+valor1 = int(input("Digite um numero:"))
+valor2 = int(input("Digite outro numero:"))
+operacao = input("Qual operação")
+
+soma = valor2 + valor2
+subtracao = valor1 - valor2
+if soma == '+':
+    print("soma")
+elif  subtracao == '-':
+    print("subtracao")
