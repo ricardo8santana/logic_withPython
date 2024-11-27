34106bb84574a6113dbed22ebe37163ec172c149
ricardo8santana <luisriccardo@proton.me>
2024-11-27 08:32:30 -0300

    logic programming with python

--- atividade05/ex07.py
+++ atividade05/ex07.py
@@ -1,9 +1,10 @@
 tabuada = int(input("Tabuada do numero:"))
-l = 10
-c = 3 
 
-while c <= tabuada:
-     print(c)
+c = 1 
+
+while c <= 10:
+     #print(`f{tabuada} x {c} = {c * tabuada}`)
+     print(tabuada, "x", c, "=", c* tabuada)
      c = c * 1
 
 
