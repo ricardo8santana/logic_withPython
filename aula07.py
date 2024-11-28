97c580c510fae566a971d85691019ac7705ae703
ricardo8santana <luisriccardo@proton.me>
2024-11-28 08:35:07 -0300

    logic programming with python

---
+++ aula07.py
@@ -0,0 +1,9 @@
+#Imprimindo todos os itens da lista
+lista = [1, 2, 3, 4, 5]
+lista_frutas = ["Maçã", "Uva", "Banana"]
+
+for numero in lista:
+    print(numero ** 2)
+
+for fruta in lista_frutas:
+    print(fruta)
